ef51299e900158121dd54d64344c7c3a6df34ec5
reesyxan <46451408+reesyxan@users.noreply.github.com>
2021-02-05 12:43:15 -0500

    first pass done

--- improvedKNN.R
+++ improvedKNN.R
@@ -1,14 +1,14 @@
 
-k = 10
 ## datadim with rows samples and cols genes
 ## latentdim with rows samples and cols genes
-improvedKNN <- function(dataclustering, latentclustering, datadim, latentdim, k){
- dim(datadim)
-  dim(latentdim)
+improvedKNN <- function(groundtruth, datadim, latentdim, k, distdata, distlatent){
   #get distance matrices for each input data
+  if (is.null(distdata)){
   distdata <- as.matrix(dist(datadim))
+  }
+  if (is.null(dist)){
   distlatent <- as.matrix(dist(latentdim))
-  
+  }
   #rank each row's neighbors
   rankdata <- apply(distdata, 1, rank)
   ranklatent <- apply(distlatent, 1, rank)
@@ -18,36 +18,61 @@ improvedKNN <- function(dataclustering, latentclustering, datadim, latentdim, k)
   dataNN <- apply(rankdata, 2, function(x) {names(x[which(x %in% 2:(k+1))])})
   latentNN <- apply(ranklatent, 2, function(x) {names(x[which(x %in% 2:(k+1))])})
 
+  dataNNmat <- matrix(0, ncol = length(rownames(rankdata)), nrow = length(rownames(rankdata)))
+  rownames(dataNNmat) <- rownames(rankdata)
+  colnames(dataNNmat) <- colnames(rankdata)
   
-  #calculate sum of the distance between each point and it's kNN (hk, gk)
-  #hk sums
-  hk <- c()
-  
-  #TODO: there's a better way to do this loop but haven't figured out how
-  for (i in 1:ncol(dataNN)){
-    hki <- sum(distdata[dataNN[,i], dataNN[,i]])
-    hk <- c(hk, hki)
-  }
-  
-  #gk sums
-  gk <- c()
-  
-  #TODO: there's a better way to do this loop but haven't figured out how
-  for (i in 1:ncol(latentNN)){
-    gki <- sum(distlatent[latentNN[,i], latentNN[,i]])
-    gk <- c(gk, gki)
+  latentNNmat <- matrix(0, ncol = length(rownames(rankdata)), nrow = length(rownames(rankdata)))
+  rownames(latentNNmat) <- rownames(rankdata)
+  colnames(latentNNmat) <- colnames(rankdata)
+
+  #make NN matrix for original data and latent
+  for (i in 1:dim(dataNN)[2]){
+    dataNNmat[dataNN[,i],i] <- 1
   }
 
+  for (i in 1:dim(latentNN)[2]){
+    latentNNmat[latentNN[,i],i] <- 1
+  }
   
+  #calculate how many of each point's neighbors are it's own kind (hk, gk)
+  
+  # labels of each class
+  names(groundtruth) <- rownames(datadim)
+  clusterlabels <- c()
+  for (i in unique(groundtruth)){
+    clusterlabels[[i]] <- names(groundtruth[groundtruth == i])
+  }
+  
+
+  #hk
+  #subset dataNNmat for each c class and count number of neighbors with same class label
+  hk <- c()
+  for (i in 1:length(clusterlabels)) {
+    hk[[i]] <- colSums(dataNNmat[clusterlabels[[i]], clusterlabels[[i]]])
+  }
+
+
+  #TODO: there's a better way to do this loop but haven't figured out how
+  gk <- c()
+  for (i in 1:length(clusterlabels)) {
+    gk[[i]] <- colSums(latentNNmat[clusterlabels[[i]], clusterlabels[[i]]])
+  }
+  
+
   #subset hk's and gk's into c cluster groups
-  n <- length(latentclustering)
-  nc <- table(latentclustering)
-  
-  
-  #sum over k's for (gk - hk)/# clusters k
-  
-  
-  
-  
-  
+  n <- length(groundtruth)
+  nc <- table(groundtruth)
+ 
+  weights <- nc/n 
+
+  #sum over c's for weight_c*sum((gk - hk)/k)
+  improvedKNN <- c()
+  for (i in 1:length(nc)){
+    part <- weights[i]*sum((gk[[i]] - hk[[i]])/k)
+    improvedKNN <- c(improvedKNN, part)
+  }
+
+  improvedKNN <- sum(improvedKNN)
+  return(improvedKNN)
 }
